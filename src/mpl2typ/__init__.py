88597feefe9adfe9e5415dfdfcb14a2c82d64685
Janek Fleper <j.fleper@mailbox.org>
2025-03-31 01:03:12 +0200

    Get stroke and marker from each line

--- src/mpl2typ/__init__.py
+++ src/mpl2typ/__init__.py
@@ -8,8 +8,8 @@ file_header = """
 #let draw-marker(points, marker: none) = {
   if marker == none { return }
 
-  let dx = marker.width / 2
-  let dy = marker.height / 2
+  let dx = if marker.has("height") { marker.height / 2 } else { 0pt }
+  let dy = if marker.has("width") { marker.width / 2 } else { 0pt }
   points.map(point => place(dx: point.at(0) - dx, dy: point.at(1) - dy, marker)).join([])
 }
 
@@ -37,10 +37,75 @@ axes_header = """
     let (x, y) = point
     return (x * xscale + xshift, y * yscale + yshift)
   }
-
-  let draw-marker = draw-marker.with(marker: circle(radius: 4pt, fill: red, stroke: black))
-  let draw-line = draw-line.with(stroke: blue + 2pt)
 """
+
+# https://matplotlib.org/stable/gallery/lines_bars_and_markers/marker_reference.html
+MARKERS = {
+    ".": "circle(\n  radius: d / 2,\n  fill: {fill},\n  stroke: {stroke}\n)",
+    "o": "circle(\n  radius: d,\n  fill: {fill},\n  stroke: {stroke}\n)",
+    "v": "polygon(\n  (-d, -d), (0pt, d), (d, -d),\n  fill: {fill},\n  stroke: {stroke}\n)",
+    "^": "polygon(\n  (0pt, -d), (-d, d), (d, d),\n  fill: {fill},\n  stroke: {stroke}\n)",
+    "<": "polygon(\n  (d, -d), (-d, 0pt), (d, d),\n  fill: {fill},\n  stroke: {stroke}\n)",
+    ">": "polygon(\n  (-d, -d), (-d, d), (d, 0pt),\n  fill: {fill},\n  stroke: {stroke}\n)",
+    "8": "polygon(\n  (-d/2, -d), (-d, -d/2), (-d, d/2), (-d/2, d), (d/2, d), (d, d/2), (d, -d/2), (d/2, -d),\n  fill: {fill},\n  stroke: {stroke}\n)",
+}
+
+
+def get_stroke(line):
+    alpha = line.get_alpha()
+    color = line.get_color()
+    if alpha is not None:
+        color += f"{int(alpha * 255):x}"
+    color = f'color.rgb("{color}")'
+
+    capstyle = line.get_dash_capstyle()
+    if capstyle == "projecting":
+        capstyle = "square"  # this is the equivalent cap style in Typst
+    joinstyle = line.get_dash_joinstyle()
+
+    offset, pattern = line._unscaled_dash_pattern
+    if pattern is None:
+        dash = '"solid"'
+    else:
+        array = ", ".join([f"{step} * thickness" for step in pattern])
+        phase = f"{offset} * thickness"
+        dash = f"(array: ({array}), phase: {phase})"
+
+    args = (
+        f"paint: {color}",
+        "thickness: thickness",
+        f'cap: "{capstyle}"',
+        f'join: "{joinstyle}"',
+        f"dash: {dash}",
+    )
+
+    return (
+        line.get_linewidth(),
+        f"stroke(\n  {',\n  '.join(args)},\n)",
+    )
+
+
+def get_marker(line):
+    alpha = line.get_alpha()
+    edgecolor = line.get_markeredgecolor()
+    if edgecolor == "k":
+        edgecolor = "#000000"
+    facecolor = line.get_markerfacecolor()
+    if facecolor == "k":
+        facecolor = "#000000"
+    if alpha is not None:
+        edgecolor += f"{int(alpha * 255):x}"
+        facecolor += f"{int(alpha * 255):x}"
+    edgewidth = line.get_markeredgewidth()
+
+    stroke = f'color.rgb("{edgecolor}") + {edgewidth}pt'
+    return (
+        line.get_markersize() / 2,
+        MARKERS[line.get_marker()].format(
+            fill=f'color.rgb("{facecolor}")',
+            stroke=stroke,
+        ),
+    )
 
 
 def function(
@@ -116,6 +181,15 @@ def axes_template(ax: mpl.axes.Axes, index: int):
     s += axes_header + "\n"
 
     for i, line in enumerate(ax.lines):
+        thickness, stroke = get_stroke(line)
+        s += textwrap.indent(f"let thickness = {thickness}pt\n", "  ")
+        s += textwrap.indent(f"let stroke-{i} = {stroke}\n\n", "  ")
+
+        size, marker = get_marker(line)
+        s += textwrap.indent(f"let d = {size}pt\n", "  ")
+        s += textwrap.indent(f"let marker-{i} = {marker}\n\n", "  ")
+
+    for i, line in enumerate(ax.lines):
         points = line.get_xydata()
         s += f"  let data-{i} = (\n"
         for x, y in points:
@@ -123,8 +197,8 @@ def axes_template(ax: mpl.axes.Axes, index: int):
         s += "  ).map(point => transform(point))\n\n"
 
     for i, line in enumerate(ax.lines):
-        s += f"  draw-line(data-{i})\n"
-        s += f"  draw-marker(data-{i})\n"
+        s += f"  draw-line(data-{i}, stroke:stroke-{i})\n"
+        s += f"  draw-marker(data-{i}, marker:marker-{i})\n"
     s += "}\n\n"
     return s
 
