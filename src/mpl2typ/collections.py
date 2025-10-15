Fix sign of y-coordinate in custom transformation
The orientation of the y-coordinate is inverted between Typst and matplotlib. The general transform() in each axes took this into account with the negated yscale. However, doing the y-axis inversion at the end of the transformation with (x, 100% - y) is a lot easier to understand.

src/mpl2typ/collections.py
@@ -88,7 +88,7 @@ class Collection:
         scale = np.diag(matrix[:2, :2])
         shift = matrix[:2, 2]
         return "point => " + typst.transform(
-            list(scale), list(shift), unit=["1pt", "1pt"]
+            list(scale), list(shift), unit=["1pt", "-1pt"]
         )
 
     @property